17da897964083200c7f805528cb806675eadae60
Connor Abdelnoor <connor.abd@gmail.com>
2015-03-06 09:59:57 -0800

    debug app and create form for inputing contacts

--- Address/app/app.php
+++ Address/app/app.php
@@ -1,23 +1,29 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/book_object";
+    require_once __DIR__."/../src/book_object.php";
 
     session_start();
-    if (empty($_SESSION['address_book']){
+    if (empty($_SESSION['list_of_contacts'])){
         $_SESSION['list_of_contacts'] = array();
-    });
+    }
 
     $app = new Silex\Application();
 
     $app->register(new Silex\Provider\TwigServiceProvider(),
-        array('twig.path' => __DIR__.'/../views');
+        array('twig.path' => __DIR__.'/../views')
     );
 
-    $app->get("/", function() {
-        return "hello";
+    $app->get("/", function() use($app){
+
+        return $app['twig']->render('booklet.php', array('contacts' => Contact::getAll()));
     });
 
+    $app->post("/new_contact", function() use($app) {
+        $new_contact = new Contact($_POST['new_name'], $_POST['new_phone'], $_POST['new_address']);
+        $new_contact->save();
 
+        return $app['twig']->render('enter_contact.php', array('newPerson' => $new_contact));
+    });
 
 
     return $app;
---
+++ Address/views/booklet.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <link rel="stylesheet" href="styles.css" type="text/css">
+    </head>
+    <body>
+        <div class="container">
+
+            <div class=form>
+                <form action="new_contact" method="post">
+                    <label for="new_name">Enter Contacts Name:</label>
+                    <input id="new_name" name="new_name" type="text">
+                    <label for="new_phone">Enter Contacts Phone:</label>
+                    <input id="new_phone" name="new_phone" type="text">
+                    <label for="new_address">Enter Contacts Address:</label>
+                    <input id="new_address" name="new_address" type="text">
+
+                    <button type="sumbit" class="button">Add your Contact</button>
+                </form>
+            </div>
+        </div>
+    </body>
+</html>
